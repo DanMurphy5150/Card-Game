e455fd30eb4c107c503ec4d5a9f589f8e72edb6c
Daniel Murphy <daniel.murphy@capco.com>
2022-04-03 22:21:54 -0400

    testing the data json files and the images --able to pull in the image

--- Card Game/src/components/GameCard.vue
+++ Card Game/src/components/GameCard.vue
@@ -1,3 +1,25 @@
 <template>
-  <h1>I am a game card</h1>
+  <button @click="readData">Click Me for data</button>
+  <ul>
+    <li v-for="data in cardData" :key="data.id">
+      <img v-bind:src="'/src/assets/images/cats/' + data.img" />
+    </li>
+  </ul>
 </template>
+
+<script>
+import CardThemes from '../data/cardThemes.json';
+
+export default {
+  data() {
+    return {
+      cardData: CardThemes,
+    };
+  },
+  methods: {
+    readData() {
+      console.log(this.CardThemes);
+    },
+  },
+};
+</script>
